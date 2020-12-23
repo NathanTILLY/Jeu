263952326d61a58934159f3096be75633ad072af
NathanTILLY <74654828+NathanTILLY@users.noreply.github.com>
2020-12-23 03:20:15 +0100

    Tous les persos peuvent choisir comme ash

--- js/rpg.js
+++ js/rpg.js
@@ -1,12 +1,14 @@
 var PVfafnir = 250;
 var ATKfafnir = 25;
+var tourPoisonFafnir = 0;
 
 var PVghisarma = 340;
 var ATKghisarma = 10;
-
+var tourPoisonGhisarma = 0;
 
 var PVlucia = 400;
 var ATKlucia = 20;
+var tourPoisonLucia = 0;
 
 var PVash = 300;
 var MANAash = 50;
@@ -14,12 +16,15 @@ var AshPare = false;
 
 var PVpenelo = 100;
 var MANApenelo = 110;
+var PeneloPare = false;
 
 var PVdarwin = 150;
 var MANAdarwin = 40;
+var DarwinPare = false;
 
 var PVviviane = 120;
 var MANAviviane = 150;
+var VivianePare = false;
 
 function afficher_aide(aide) {
 	if (aide.style.display == "none") aide.style.display = "block";
@@ -229,6 +234,569 @@ var btn_repos_viviane = document.getElementById(btn_repos_Viviane);
 	} 
 		
 	/* ----------------------- FIN ASH -------------------------------*/ 
+
+	/* ----------------------- PENELO --------------------------------*/
+
+	btn_attaquer_Penelo.onclick = function()
+	{
+		btn_attaquer_Penelo.style.display = "none";
+		btn_special_Penelo.style.display = "none";
+		btn_parer_Penelo.style.display = "none";
+		btn_repos_Penelo.style.display = "none";
+
+		btn_targetFafnir_Penelo.style.display = "block";
+		btn_targetLucia_Penelo.style.display = "block";
+		btn_targetGhisarma_Penelo.style.display = "block";
+		btn_retour_Penelo.style.display = "block";
+
+		btn_targetFafnir_Penelo.onclick = function()
+		{
+			PVfafnir = PVfafnir - 10;
+			btn_targetFafnir_Penelo.style.display = "none";
+			btn_targetLucia_Penelo.style.display = "none";
+			btn_targetGhisarma_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+
+		}
+		btn_targetLucia_Penelo.onclick = function()
+		{
+			PVlucia = PVlucia - 10;
+			btn_targetFafnir_Penelo.style.display = "none";
+			btn_targetLucia_Penelo.style.display = "none";
+			btn_targetGhisarma_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+		btn_targetGhisarma_Penelo.onclick = function()
+		{
+			PVghisarma = PVghisarma - 10;
+			btn_targetFafnir_Penelo.style.display = "none";
+			btn_targetLucia_Penelo.style.display = "none";
+			btn_targetGhisarma_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+		btn_retour_Penelo.onclick = function()
+		{
+			btn_attaquer_Penelo.style.display = "block";
+			btn_special_Penelo.style.display = "block";
+			btn_parer_Penelo.style.display = "block";
+			btn_repos_Penelo.style.display = "block";
+
+			btn_targetFafnir_Penelo.style.display = "none";
+			btn_targetLucia_Penelo.style.display = "none";
+			btn_targetGhisarma_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+	} 
+
+
+	btn_special_Penelo.onclick = function()
+	{
+		btn_attaquer_Penelo.style.display = "none";
+		btn_special_Penelo.style.display = "none";
+		btn_parer_Penelo.style.display = "none";
+		btn_repos_Penelo.style.display = "none";
+
+		btn_targetAsh_Penelo.style.display = "block";
+		btn_targetPenelo_Penelo.style.display = "block";
+		btn_targetDarwin_Penelo.style.display = "block";
+		btn_targetViviane_Penelo.style.display = "block";
+		btn_retour_Penelo.style.display = "block";
+
+		btn_targetAsh_Penelo.onclick = function()
+		{
+			PVash = PVash + 40;
+			MANApenelo = MANApenelo -20;
+
+			btn_targetAsh_Penelo.style.display = "none";
+			btn_targetPenelo_Penelo.style.display = "none";
+			btn_targetDarwin_Penelo.style.display = "none";
+			btn_targetViviane_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+
+		}
+		btn_targetPenelo_Penelo.onclick = function()
+		{
+			PVpenelo = PVpenelo + 40;
+			MANApenelo = MANApenelo -20;
+
+			btn_targetAsh_Penelo.style.display = "none";
+			btn_targetPenelo_Penelo.style.display = "none";
+			btn_targetDarwin_Penelo.style.display = "none";
+			btn_targetViviane_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+		btn_targetDarwin_Penelo.onclick = function()
+		{
+			PVdarwin = PVdarwin + 40;
+			MANApenelo = MANApenelo -20;
+
+			btn_targetAsh_Penelo.style.display = "none";
+			btn_targetPenelo_Penelo.style.display = "none";
+			btn_targetDarwin_Penelo.style.display = "none";
+			btn_targetViviane_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+		btn_targetViviane_Penelo.onclick = function()
+		{
+			PVviviane = PVviviane + 40;
+			MANApenelo = MANApenelo -20;
+
+			btn_targetAsh_Penelo.style.display = "none";
+			btn_targetPenelo_Penelo.style.display = "none";
+			btn_targetDarwin_Penelo.style.display = "none";
+			btn_targetViviane_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+		btn_retour_Penelo.onclick = function()
+		{
+			btn_attaquer_Penelo.style.display = "block";
+			btn_special_Penelo.style.display = "block";
+			btn_parer_Penelo.style.display = "block";
+			btn_repos_Penelo.style.display = "block";
+
+			btn_targetAsh_Penelo.style.display = "none";
+			btn_targetPenelo_Penelo.style.display = "none";
+			btn_targetDarwin_Penelo.style.display = "none";
+			btn_targetViviane_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+	} 
+
+
+	btn_parer_Penelo.onclick = function()
+	{
+		btn_attaquer_Penelo.style.display = "none";
+		btn_special_Penelo.style.display = "none";
+		btn_parer_Penelo.style.display = "none";
+		btn_repos_Penelo.style.display = "none";
+
+		btn_confirmer_Penelo.style.display = "block";
+		btn_retour_Penelo.style.display = "block";
+
+		btn_confirmer_Penelo.onclick = function()
+		{
+			PeneloPare = true;
+
+			btn_confirmer_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+
+		btn_retour_Penelo.onclick = function()
+		{
+			btn_attaquer_Penelo.style.display = "block";
+			btn_special_Penelo.style.display = "block";
+			btn_parer_Penelo.style.display = "block";
+			btn_repos_Penelo.style.display = "block";
+
+			btn_targetFafnir_Penelo.style.display = "none";
+			btn_targetLucia_Penelo.style.display = "none";
+			btn_targetGhisarma_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+			btn_confirmer_Penelo.style.display = "none";
+		}
+	} 
+
+
+	btn_repos_Penelo.onclick = function()
+	{
+		btn_attaquer_Penelo.style.display = "none";
+		btn_special_Penelo.style.display = "none";
+		btn_parer_Penelo.style.display = "none";
+		btn_repos_Penelo.style.display = "none";
+
+		btn_confirmer_Penelo.style.display = "block";
+		btn_retour_Penelo.style.display = "block";
+
+		btn_confirmer_Penelo.onclick = function()
+		{
+			PVpenelo = PVpenelo + 10;
+
+			btn_confirmer_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+		}
+		btn_retour_Penelo.onclick = function()
+		{
+			btn_attaquer_Penelo.style.display = "block";
+			btn_special_Penelo.style.display = "block";
+			btn_parer_Penelo.style.display = "block";
+			btn_repos_Penelo.style.display = "block";
+
+			btn_targetFafnir_Penelo.style.display = "none";
+			btn_targetLucia_Penelo.style.display = "none";
+			btn_targetGhisarma_Penelo.style.display = "none";
+			btn_retour_Penelo.style.display = "none";
+			btn_confirmer_Penelo.style.display = "none";
+		}
+	} 
+
+	/* ----------------------- FIN PENELO --------------------------------*/
+
+	/* ----------------------- DARWIN --------------------------------*/
+
+	btn_attaquer_Darwin.onclick = function()
+	{
+		btn_attaquer_Darwin.style.display = "none";
+		btn_special_Darwin.style.display = "none";
+		btn_parer_Darwin.style.display = "none";
+		btn_repos_Darwin.style.display = "none";
+
+		btn_targetFafnir_Darwin.style.display = "block";
+		btn_targetLucia_Darwin.style.display = "block";
+		btn_targetGhisarma_Darwin.style.display = "block";
+		btn_retour_Darwin.style.display = "block";
+
+		btn_targetFafnir_Darwin.onclick = function()
+		{
+			PVfafnir = PVfafnir - 35;
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+
+		}
+		btn_targetLucia_Darwin.onclick = function()
+		{
+			PVlucia = PVlucia - 35;
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+		btn_targetGhisarma_Darwin.onclick = function()
+		{
+			PVghisarma = PVghisarma - 35;
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+		btn_retour_Darwin.onclick = function()
+		{
+			btn_attaquer_Darwin.style.display = "block";
+			btn_special_Darwin.style.display = "block";
+			btn_parer_Darwin.style.display = "block";
+			btn_repos_Darwin.style.display = "block";
+
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+	} 
+
+
+	btn_special_Darwin.onclick = function()
+	{
+		btn_attaquer_Darwin.style.display = "none";
+		btn_special_Darwin.style.display = "none";
+		btn_parer_Darwin.style.display = "none";
+		btn_repos_Darwin.style.display = "none";
+
+		btn_targetFafnir_Darwin.style.display = "block";
+		btn_targetLucia_Darwin.style.display = "block";
+		btn_targetGhisarma_Darwin.style.display = "block";
+		btn_retour_Darwin.style.display = "block";
+
+		btn_targetFafnir_Darwin.onclick = function()
+		{
+			PVfafnir = PVfafnir - 80;
+			MANAdarwin = MANAdarwin -20;
+
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+
+		}
+		btn_targetLucia_Darwin.onclick = function()
+		{
+			PVlucia = PVlucia - 80;
+			MANAdarwin = MANAdarwin -20;
+
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+		btn_targetGhisarma_Darwin.onclick = function()
+		{
+			PVghisarma = PVghisarma - 80;
+			MANAdarwin = MANAdarwin -20;
+			
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+		btn_retour_Darwin.onclick = function()
+		{
+			btn_attaquer_Darwin.style.display = "block";
+			btn_special_Darwin.style.display = "block";
+			btn_parer_Darwin.style.display = "block";
+			btn_repos_Darwin.style.display = "block";
+
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+	} 
+
+
+	btn_parer_Darwin.onclick = function()
+	{
+		btn_attaquer_Darwin.style.display = "none";
+		btn_special_Darwin.style.display = "none";
+		btn_parer_Darwin.style.display = "none";
+		btn_repos_Darwin.style.display = "none";
+
+		btn_confirmer_Darwin.style.display = "block";
+		btn_retour_Darwin.style.display = "block";
+
+		btn_confirmer_Darwin.onclick = function()
+		{
+			DarwinPare = true;
+
+			btn_confirmer_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+
+		btn_retour_Darwin.onclick = function()
+		{
+			btn_attaquer_Darwin.style.display = "block";
+			btn_special_Darwin.style.display = "block";
+			btn_parer_Darwin.style.display = "block";
+			btn_repos_Darwin.style.display = "block";
+
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+			btn_confirmer_Darwin.style.display = "none";
+		}
+	} 
+
+
+	btn_repos_Darwin.onclick = function()
+	{
+		btn_attaquer_Darwin.style.display = "none";
+		btn_special_Darwin.style.display = "none";
+		btn_parer_Darwin.style.display = "none";
+		btn_repos_Darwin.style.display = "none";
+
+		btn_confirmer_Darwin.style.display = "block";
+		btn_retour_Darwin.style.display = "block";
+
+		btn_confirmer_Darwin.onclick = function()
+		{
+			PVdarwin = PVdarwin + 10;
+
+			btn_confirmer_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+		}
+		btn_retour_Darwin.onclick = function()
+		{
+			btn_attaquer_Darwin.style.display = "block";
+			btn_special_Darwin.style.display = "block";
+			btn_parer_Darwin.style.display = "block";
+			btn_repos_Darwin.style.display = "block";
+
+			btn_targetFafnir_Darwin.style.display = "none";
+			btn_targetLucia_Darwin.style.display = "none";
+			btn_targetGhisarma_Darwin.style.display = "none";
+			btn_retour_Darwin.style.display = "none";
+			btn_confirmer_Darwin.style.display = "none";
+		}
+	} 
+
+	/* ----------------------- FIN DARWIN --------------------------------*/
+
+	/* ----------------------- VIVIANE --------------------------------*/
+
+	btn_attaquer_Viviane.onclick = function()
+	{
+		btn_attaquer_Viviane.style.display = "none";
+		btn_special_Viviane.style.display = "none";
+		btn_parer_Viviane.style.display = "none";
+		btn_repos_Viviane.style.display = "none";
+
+		btn_targetFafnir_Viviane.style.display = "block";
+		btn_targetLucia_Viviane.style.display = "block";
+		btn_targetGhisarma_Viviane.style.display = "block";
+		btn_retour_Viviane.style.display = "block";
+
+		btn_targetFafnir_Viviane.onclick = function()
+		{
+			PVfafnir = PVfafnir - 15;
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+
+		}
+		btn_targetLucia_Viviane.onclick = function()
+		{
+			PVlucia = PVlucia - 15;
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+		btn_targetGhisarma_Viviane.onclick = function()
+		{
+			PVghisarma = PVghisarma - 15;
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+		btn_retour_Viviane.onclick = function()
+		{
+			btn_attaquer_Viviane.style.display = "block";
+			btn_special_Viviane.style.display = "block";
+			btn_parer_Viviane.style.display = "block";
+			btn_repos_Viviane.style.display = "block";
+
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+	} 
+
+
+	btn_special_Viviane.onclick = function()
+	{
+		btn_attaquer_Viviane.style.display = "none";
+		btn_special_Viviane.style.display = "none";
+		btn_parer_Viviane.style.display = "none";
+		btn_repos_Viviane.style.display = "none";
+
+		btn_targetFafnir_Viviane.style.display = "block";
+		btn_targetLucia_Viviane.style.display = "block";
+		btn_targetGhisarma_Viviane.style.display = "block";
+		btn_retour_Viviane.style.display = "block";
+
+		btn_targetFafnir_Viviane.onclick = function()
+		{
+			
+			MANAviviane = MANAviviane -35;
+			tourPoisonFafnir = 3;
+
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+
+		}
+		btn_targetLucia_Viviane.onclick = function()
+		{
+			
+			MANAviviane = MANAviviane -35;
+			tourPoisonLucia = 3;
+
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+		btn_targetGhisarma_Viviane.onclick = function()
+		{
+			
+			MANAviviane = MANAviviane -35;
+			tourPoisonGhisarma = 3;
+			
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+		btn_retour_Viviane.onclick = function()
+		{
+			btn_attaquer_Viviane.style.display = "block";
+			btn_special_Viviane.style.display = "block";
+			btn_parer_Viviane.style.display = "block";
+			btn_repos_Viviane.style.display = "block";
+
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+	} 
+
+
+	btn_parer_Viviane.onclick = function()
+	{
+		btn_attaquer_Viviane.style.display = "none";
+		btn_special_Viviane.style.display = "none";
+		btn_parer_Viviane.style.display = "none";
+		btn_repos_Viviane.style.display = "none";
+
+		btn_confirmer_Viviane.style.display = "block";
+		btn_retour_Viviane.style.display = "block";
+
+		btn_confirmer_Viviane.onclick = function()
+		{
+			VivianePare = true;
+
+			btn_confirmer_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+
+		btn_retour_Viviane.onclick = function()
+		{
+			btn_attaquer_Viviane.style.display = "block";
+			btn_special_Viviane.style.display = "block";
+			btn_parer_Viviane.style.display = "block";
+			btn_repos_Viviane.style.display = "block";
+
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+			btn_confirmer_Viviane.style.display = "none";
+		}
+	} 
+
+
+	btn_repos_Viviane.onclick = function()
+	{
+		btn_attaquer_Viviane.style.display = "none";
+		btn_special_Viviane.style.display = "none";
+		btn_parer_Viviane.style.display = "none";
+		btn_repos_Viviane.style.display = "none";
+
+		btn_confirmer_Viviane.style.display = "block";
+		btn_retour_Viviane.style.display = "block";
+
+		btn_confirmer_Viviane.onclick = function()
+		{
+			PVviviane = PVviviane + 10;
+
+			btn_confirmer_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+		}
+		btn_retour_Viviane.onclick = function()
+		{
+			btn_attaquer_Viviane.style.display = "block";
+			btn_special_Viviane.style.display = "block";
+			btn_parer_Viviane.style.display = "block";
+			btn_repos_Viviane.style.display = "block";
+
+			btn_targetFafnir_Viviane.style.display = "none";
+			btn_targetLucia_Viviane.style.display = "none";
+			btn_targetGhisarma_Viviane.style.display = "none";
+			btn_retour_Viviane.style.display = "none";
+			btn_confirmer_Viviane.style.display = "none";
+		}
+	} 
+
+	/* ----------------------- FIN VIVIANE --------------------------------*/
+
+
+
+
 /*}
 
 /if(PVash==0 & PVpenelo==0 & PVdarwin==0 & PVviviane==0)
